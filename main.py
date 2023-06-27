980c8332960290db2fd11ec5feeac0a48204be45
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-27 21:21:53 +0300

    t

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+answer = input("Enter your answer: ")
+correct_answer = input("Enter the correct one: ")
+def question(correct_answer, answer):
+    if correct_answer == answer:
+        print("you are the winner")
+    elif correct_answer != answer:
+        pass
+    elif correct_answer != answer:
+        pass
+    elif correct_answer != answer:
 
-text = input("Enter string: ")
-
-text_1 = list(text)
-text_1.sort()
-print(text_1)
-
-
-
-# def sort_alph(text):
-#     text =
-
-#     print(text)
-
+        print("Game over")
 
 
