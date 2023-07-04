08daf4b0810acc4489b6f830cf03ae6ea82ecb4f
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-07-04 19:16:06 +0300

    lesson 10

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-answer =
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    def display_employee(self):
+        print(name, salary)
+
+display = Employee(input("Enter name "), input("Enter salary "))
