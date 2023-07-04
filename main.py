d2e2c1c5317276a671435f0db595e96f8532f476
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-07-04 19:19:04 +0300

    lesson 10

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ class Employee:
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
-    def display_employee(self):
-        print(name, salary)
 
 
-display = Employee(input("Enter name "), input("Enter salary "))
+
+display_employee = Employee(input("Enter name "), input("Enter salary "))
