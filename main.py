f69b9116ee23dfe5910e5809c8e83c8f858b9330
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-07-04 18:53:04 +0300

    t

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-answer = input("Enter your answer: ")
-correct_answer = input("Enter the correct one: ")
-if answer == correct_answer:
-    print("you are the winner")
-if answer != correct_answer:
-    print("2 attempts left")
-answer = input("Enter your answer: ")
-if answer != correct_answer:
-    print("1 attempt left")
-if answer == correct_answer:
-    print("you are the winner")
-answer = input("Enter your answer: ")
-if answer != correct_answer:
-    print("failed")
-if answer == correct_answer:
-    print("you are the winner")
+answer
