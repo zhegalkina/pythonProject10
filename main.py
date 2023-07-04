980c08cca8516cad16b5aee568a639c73374ce96
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-07-04 21:39:04 +0300

    lesson 10

--- main.py
+++ main.py
@@ -19,19 +19,48 @@
 # emp2.display_count()
 
 
-class Employee:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
+# class Employee:
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
+#
+# class Freelance(Employee):
+#     def __init__(self, name, salary, email):
+#         super().__init__(name, salary)
+#         self.email = email
+#     def display_email(self):
+#         print("Name:", self.name, "salary", self.salary, "email", self.email)
+#
+# example = Freelance("Tom", 5000, "email")
+# example.display_email()
 
-class Freelance(Employee):
-    def __init__(self, name, salary, email):
-        super().__init__(name, salary)
-        self.email = email
-    def display_email(self):
-        print("Name:", self.name, "salary", self.salary, "email", self.email)
 
-example = Freelance("Tom", 5000, "email")
-example.display_email()
+class Animal:
+    def __init__(self, legs, tail, domestic, mammals):
+        self.legs = legs
+        self.tail = tail
+        self.domestic = domestic
+        self.mammals = mammals
+    def is_animals(self):
+        if self.mammals == True:
+            print("mammals")
+        else:
+            print("not mammals")
+
+    def is_domestic(self):
+        if self.domestic == True:
+            print("domestic")
+        else:
+            print("not domestic")
+
+example = Animal(4, True, True, True)
+example.is_animals()
+example.is_domestic()
+
+
+
+
+
+
 
 
