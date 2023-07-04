8f297caa5dfdc880c495d57bb69d08d88ed40c94
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-07-04 19:55:36 +0300

    lesson 10

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
+# class Employee:
+#
+#     emp_count = 0
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
+#         Employee.emp_count += 1
+#     def display_employee(self):
+#         print("Name:", self.name, "salary", self.salary)
+#
+#     def display_count(self):
+#         print("All", Employee.emp_count)
+#
+# emp = Employee("Tom", 5000)
+#
+# emp.display_employee()
+# emp.display_count()
+# emp2 = Employee("Anton", 4000)
+# emp2.display_count()
+
+
 class Employee:
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
-    def display_employee(self):
-        print(self.name, self.salary)
 
-display = Employee(input("Enter name: "), (input("Enter salary: ")))
+class Freelance(Employee):
+    def __init__(self, name, salary, email):
+        super.__init__(name, salary)
+        self.email = email
+    def display_email(self):
+        print("Name:", self.name, "salary", self.salary, "email", self.email)
 
-display.display_employee()
+example = Freelance("Tom", 5000, "email")
+example.display_email()
+
+
